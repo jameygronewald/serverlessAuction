d3eea79024510b7b2c3ac0319d28cd82f15cfd16
Jamey Gronewald <jrgronewald@gmail.com>
2020-10-27 16:55:00 -0700

    validation for auction pictures so only the seller can upload an image url; also checking base64 w regex

--- auctionService/src/handlers/uploadAuctionPicture.js
+++ auctionService/src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,44 @@
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import validator from "@middy/validator";
+import cors from "@middy/http-cors";
+import createError from "http-errors";
+import { getAuctionById } from "./getAuction";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
+import uploadAuctionPictureSchema from "../lib/schemas/uploadAuctionPictureSchema";
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
+  const auction = await getAuctionById(id);
+
+  if (email !== auction.seller) {
+    throw new createError.Forbidden(
+      `Only the seller can upload a picture to an auction.`
+    );
+  }
+
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64, "base64");
+
+  let updatedAuction;
+
+  try {
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }))
+  .use(cors());
--- auctionService/src/lib/commonMiddleware.js
+++ auctionService/src/lib/commonMiddleware.js
@@ -2,10 +2,12 @@ import middy from "@middy/core";
 import httpJSONBodyParser from "@middy/http-json-body-parser";
 import httpEventNormalizer from "@middy/http-event-normalizer";
 import httpErrorHandler from "@middy/http-error-handler";
+import cors from "@middy/http-cors";
 
-export default handler => middy(handler)
-    .use([
-        httpJSONBodyParser(),
-        httpEventNormalizer(),
-        httpErrorHandler()
-    ]);
+export default handler =>
+  middy(handler).use([
+    httpJSONBodyParser(),
+    httpEventNormalizer(),
+    httpErrorHandler(),
+    cors(),
+  ]);
